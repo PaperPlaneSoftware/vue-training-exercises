f7fb5311f1de48c77761f4391ee4f25ad51ab1e9
Oliver Winks <oliver@paperplane.software>
2019-06-13 16:52:10 +0100

    Add day 2 - exercise iv
     Named slots example

---
+++ day_2/4_card-component/src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <card>
+      <template slot="header">
+        <span>🖖</span> <span class="title">Live Long and Prosper</span>
+        <hr/>
+      </template>
+      <template slot="body">
+        Some profound Vulcan saying...
+      </template>
+    </card>
+  </div>
+</template>
+
+<script>
+import Card from './components/Card';
+
+export default {
+  name: 'app',
+  components: { Card }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ day_2/4_card-component/src/components/Card.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="card-container">
+        <div class="header">
+            <slot name="header">Title!</slot>
+        </div>
+        <div class="body">
+            <slot name="body">Nothing to show!</slot>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Card'
+}
+</script>
+
+<style scoped>
+.card-container {
+    display: flex;
+    flex-direction: column;
+
+    border: 4px solid #3e3e3e;
+    border-radius: 8px;
+
+    width: 200px;
+    margin: 16px;
+}
+
+.header {
+    font-size: 16pt;
+    font-weight: bold;
+
+    margin-bottom: 16px;
+}
+
+.body {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 16px;
+}
+</style>
